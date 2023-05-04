563e8a4ddc94f4af9015f42d9691a2ad0cfd6756
Season0518 <1342367762@qq.com>
2023-05-04 22:28:48 +0800

    修复了缺失的文件夹

---
+++ vscode/src/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define dbg(...) __VA_ARGS__
+#endif
+
+constexpr int INF=0x3f3f3f3f;
+constexpr int MAX=1000;
+
+int main(void){
+    
+    
+    return 0;
+}
